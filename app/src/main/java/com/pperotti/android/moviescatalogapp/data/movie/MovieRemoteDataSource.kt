7ee3c44f537109d9b28934e20f719d0565f5600a
Pablo Perotti <pablo.perotti@gmail.com>
2025-02-04 15:32:58 -0300

    Update Api to include the retrieval of movie details.

--- app/src/main/java/com/pperotti/android/moviescatalogapp/data/movie/MovieRemoteDataSource.kt
+++ app/src/main/java/com/pperotti/android/moviescatalogapp/data/movie/MovieRemoteDataSource.kt
@@ -6,12 +6,28 @@ import javax.inject.Singleton
 interface MovieRemoteDataSource {
 
     /**
-     * Retrieve the list of movies from the network
+     * Retrieve the list of movies from the network.
      *
      * @param includeAdult Determine whether it includes adult files or not
      * @param includeVideo Determine whether it includes information about an associated video.
+     * @param page The value used by TMDB to determine the list of movies it sends.
+     *
+     * @return One page of movies encapsulated within the MovieListResult
      */
-    suspend fun fetchMovieList(includeAdult: Boolean = false, includeVideo: Boolean = false): MovieListResult
+    suspend fun fetchMovieList(
+        includeAdult: Boolean = false,
+        includeVideo: Boolean = false,
+        page: Int = 1
+    ): MovieListResult
+
+    /**
+     * Retrieve the full details for a movie given an id.
+     *
+     * @param The ID for the selected movie.
+     * @return All the details TMDB has associated with this movie
+     *         encapsulated in MovieDetails class
+     */
+    suspend fun fetchMovieDetails(id: Int): MovieDetails
 }
 
 @Singleton
@@ -19,7 +35,15 @@ class DefaultMovieRemoteDataSource @Inject constructor(
     val tmdbApi: TmdbApi
 ) : MovieRemoteDataSource {
 
-    override suspend fun fetchMovieList(includeAdult: Boolean, includeVideo: Boolean): MovieListResult {
-        return tmdbApi.fetchMovieList(includeAdult, includeVideo)
+    override suspend fun fetchMovieList(
+        includeAdult: Boolean,
+        includeVideo: Boolean,
+        page: Int
+    ): MovieListResult {
+        return tmdbApi.fetchMovieList(includeAdult, includeVideo, page)
+    }
+
+    override suspend fun fetchMovieDetails(id: Int): MovieDetails {
+        return tmdbApi.fetchMovieDetails(id)
     }
 }
---
+++ app/src/main/java/com/pperotti/android/moviescatalogapp/presentation/details/DetailsScreen.kt
@@ -0,0 +1,4 @@
+package com.pperotti.android.moviescatalogapp.presentation.details
+
+class DetailsScreen {
+}
